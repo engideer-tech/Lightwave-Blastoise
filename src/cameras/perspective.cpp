b18551721006dac80d4fabf14a03a7448999db7b
Michal <s8mijure@stud.uni-saarland.de>
2023-11-13 16:12:27 +0100

    s1e1.1: support fov on y axis

--- src/cameras/perspective.cpp
+++ src/cameras/perspective.cpp
@@ -14,38 +14,41 @@ namespace lightwave {
 class Perspective : public Camera {
 private:
     static constexpr float degToPi = Pi / 180.0f;
-    static constexpr float alp2 = 90.0f * degToPi;
+    static constexpr float angle2 = 90.0f * degToPi;
     Matrix3x3 tfToCameraCoordsSys{};
 
 public:
-    Perspective(const Properties &properties) : Camera(properties) {
-        const float x1 = 0.0f, y1 = 0.0f;
-        const float x2 = 0.0f, y2 = 1.0f;
+    /**
+     * Precomputes transformation matrix from normalized image plane coordinates to local camera coordinate system,
+     * where the image plane is at z = 1. This is done by spanning a triangle in the camera's coordinate system,
+     * where p1 = camera, p2 = center of plane, and p3 = edge of image plane on the fovAxis. p1 and p2 are known,
+     * p3 is computed with Pythagoras & law of sines, given we know all the triangle's angles.
+     */
+    explicit Perspective(const Properties &properties) : Camera(properties) {
         const float fov = properties.get<float>("fov");
-        const float alp1 = (fov / 2) * degToPi;
+        const std::string fovAxis = properties.get<std::string>("fovAxis");
 
-        const float u = x2 - x1, v = y2 - y1;
+        const float p1x = 0.0f, p1y = 0.0f;
+        const float p2x = 0.0f, p2y = 1.0f;
+        const float angle1 = (fov / 2) * degToPi;
+
+        const float u = p2x - p1x, v = p2y - p1y;
         const float a3 = sqrt(u * u + v * v);
-        const float alp3 = Pi - (alp1 + alp2);
-        const float a2 = a3 * std::sin(alp2) / std::sin(alp3);
-        const float RHS1 = x1 * u - y1 * v + a2 * a3 * std::cos(alp1);
-        const float RHS2 = y2 * u - x2 * v - a2 * a3 * std::sin(alp1);
-        const float x3 = (1 / (a3 * a3)) * (u * RHS1 - v * RHS2);
+        const float angle3 = Pi - (angle1 + angle2);
+        const float a2 = a3 * std::sin(angle2) / std::sin(angle3);
+        const float RHS1 = p1x * u - p1y * v + a2 * a3 * std::cos(angle1);
+        const float RHS2 = p2y * u - p2x * v - a2 * a3 * std::sin(angle1);
+        const float p3x = (1 / (a3 * a3)) * (u * RHS1 - v * RHS2);
 
-        // TODO: calculate differently based on axis of fov
         const float resolutionRatio = static_cast<float>(m_resolution.x()) / static_cast<float>(m_resolution.y());
-        const float y3 = x3 / resolutionRatio;
+        const float y3 = (fovAxis == "x") != (m_resolution.x() < m_resolution.y()) ? // XOR
+                         p3x / resolutionRatio : p3x * resolutionRatio;
 
-        std::cout << "x3: " << x3 << " y3: " << y3 << "\n";
+        std::cout << "p3x: " << p3x << " y3: " << y3 << "\n";
 
-        tfToCameraCoordsSys.setColumn(0, Vector(x3, 0.0f, 0.0f));
-        tfToCameraCoordsSys.setColumn(1, Vector(0.0f, y3, 0.0f));
+        tfToCameraCoordsSys.setColumn(0, Vector(fovAxis == "x" ? p3x : y3, 0.0f, 0.0f));
+        tfToCameraCoordsSys.setColumn(1, Vector(0.0f, fovAxis == "x" ? y3 : p3x, 0.0f));
         tfToCameraCoordsSys.setColumn(2, Vector(0.0f, 0.0f, 1.0f));
-
-        // const Point2 point{-0.435041, 0.216175};
-        // const Vector directionInCameraCoords = tfToCameraCoordsSys * Vector(point.x(), point.y(), 0.0f);
-        // std::cout << "matrix " << tfToCameraCoordsSys << "\n";
-        // std::cout << "point " << point << " direction " << directionInCameraCoords << "\n";
     }
 
     CameraSample sample(const Point2 &normalized, Sampler &rng) const override {
