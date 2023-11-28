6c5941859cc5866d1f4d8607c9754735ebc1c0f6
Chinmay Joshi <chjo00003@stud.uni-saarland.de>
2023-11-28 13:05:46 +0100

    Includes direct integrtor, and diffuse.cpp is incomplete

--- src/bsdfs/diffuse.cpp
+++ src/bsdfs/diffuse.cpp
@@ -12,7 +12,17 @@ public:
 
     BsdfSample sample(const Point2 &uv, const Vector &wo,
                       Sampler &rng) const override {
-        NOT_IMPLEMENTED
+        //NOT_IMPLEMENTED
+        Vector outgoing_direction = squareToCosineHemisphere(Point2(wo.x(),wo.y())).normalized();
+        Vector wi = squareToCosineHemisphere(rng.next2D()).normalized();
+        float pdf = cosineHemispherePdf(wi)/(2*Pi);
+        Color bsdf_val = m_albedo->evaluate(uv);
+        bsdf_val *= (outgoing_direction.z()*wi.z())/pdf;
+
+        BsdfSample sample;
+        sample.wi = wi;
+        sample.weight = bsdf_val;
+        return sample;
     }
 
     std::string toString() const override {
---
+++ src/integrators/direct.cpp
@@ -0,0 +1,64 @@
+//
+// Created by chinm on 26-11-2023.
+//
+#include <lightwave.hpp>
+
+namespace lightwave {
+/**
+ * Renders objects by using their surface normal coordinates as RGB color values.
+ */
+    class DirectIntegrator : public SamplingIntegrator {
+    private:
+        bool remap;
+
+    public:
+        explicit DirectIntegrator(const Properties& properties) : SamplingIntegrator(properties) {
+            remap = properties.get<bool>("remap", true);
+        }
+
+        Color Li(const Ray& ray, Sampler& rng) override {
+            Ray currentRay = ray;
+            Color finalColor(0.0f); // Initialize final color
+            int maxBounces = 1;
+            for (int bounce = 0; bounce < maxBounces; ++bounce) {
+                Intersection intersection = m_scene->intersect(currentRay, rng);
+
+                if (!intersection) {
+                    // No intersection; return background color
+                    BackgroundLightEval backgroundLightEval = m_scene->evaluateBackground((currentRay.direction));
+                    finalColor = finalColor + backgroundLightEval.value;
+                    break;
+                }
+
+                // Sample the BSDF
+                BsdfSample bsdfSample = intersection.sampleBsdf(rng);
+
+                // Update the ray direction and weight based on the BSDF sample
+                currentRay = Ray(intersection.position, bsdfSample.wi);
+                finalColor += bsdfSample.weight; // Accumulate contribution
+
+                // Check if the secondary ray escapes the scene
+                if (!m_scene->intersect(currentRay, rng)) {
+                    BackgroundLightEval backgroundLightEval = m_scene->evaluateBackground((currentRay.direction));
+                    finalColor = finalColor + backgroundLightEval.value;
+                    break;
+                }
+            }
+
+            return finalColor;
+        }
+
+        std::string toString() const override {
+            return tfm::format(
+                    "DirectIntegrator[\n"
+                    "  sampler = %s,\n"
+                    "  image = %s,\n"
+                    "]",
+                    indent(m_sampler),
+                    indent(m_image)
+            );
+        }
+    };
+}
+REGISTER_INTEGRATOR(DirectIntegrator, "direct")
+
