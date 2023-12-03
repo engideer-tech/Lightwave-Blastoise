8e31aa6b7948ec9c663ef873d7e7ca9a67a6bbab
Chinmay Joshi <chjo00003@stud.uni-saarland.de>
2023-12-03 17:54:19 +0100

    updated bilinear, yet broken image.cpp

--- src/textures/image.cpp
+++ src/textures/image.cpp
@@ -39,7 +39,7 @@ public:
 
     Color evaluate(const Point2& uv) const override {
         float u = uv.x();
-        float v = 1 - uv.y();
+        float v = 1.0-uv.y();
 
         switch (m_border) {
             case BorderMode::Clamp:
@@ -54,49 +54,59 @@ public:
                 // v = std::fmod(v, m_image->resolution().y());
                 u = std::fmod(u, 1.0f);
                 v = std::fmod(v, 1.0f);
+                if (u < 0) u += 1.0f;
+                if (v < 0) v += 1.0f;
                 break;
         }
 
-        const float tu = u * static_cast<float>(m_image->resolution().x()) - 1.0f;
-        const float tv = v * static_cast<float>(m_image->resolution().y()) - 1.0f;
+        const int tu = static_cast<int>(u * static_cast<float>(m_image->resolution().x()));
+        const int tv = static_cast<int>(v * static_cast<float>(m_image->resolution().y()));
 
         if (m_filter == FilterMode::Nearest) {
-            int newx = static_cast<int>(floorf(tu));
-            int newy = static_cast<int>(floorf(tv));
+//            int newx = static_cast<int>(floorf(tu));
+//            int newy = static_cast<int>(floorf(tv));
+//
+//            const float diffx = tu - static_cast<float>(newx);
+//            const float diffy = tv - static_cast<float>(newy);
+//
+//            if (diffx > 0.5) newx++;
+//            if (diffy > 0.5) newy++;
+//
+//            return m_image->get(Point2i(tu, tv))*m_exposure;
 
-            const float diffx = tu - static_cast<float>(newx);
-            const float diffy = tv - static_cast<float>(newy);
+             int nearestU = static_cast<int>(std::round(tu));
+             int nearestV = static_cast<int>(std::round(tv));
 
-            if (diffx > 0.5) newx++;
-            if (diffy > 0.5) newy++;
-
-            return m_image->get(Point2i(newx, newy));
-
-            // int nearestU = static_cast<int>(std::round(u_texel));
-            // int nearestV = static_cast<int>(std::round(v_texel));
-            // // std::cout<<"interpolated color nearest"<<m_image->get(Point2i(nearestU,nearestV))*m_exposure<<std::endl;
-            // return m_image->get(Point2i(nearestU,nearestV))*m_exposure;
+             //std::cout<<"interpolated color nearest"<<m_image->get(Point2i(nearestU,nearestV))*m_exposure<<std::endl;
+             return m_image->get(Point2i(nearestU,nearestV))*m_exposure;
         } else if (m_filter == FilterMode::Bilinear) {
-            const int u0 = static_cast<int>(floorf(tu));
-            const int v0 = static_cast<int>(floorf(tv));
-            const int u1 = std::min(u0 + 1, m_image->resolution().x() - 1);
-            const int v1 = std::min(v0 + 1, m_image->resolution().y() - 1);
+//            const int u0 = static_cast<int>(floorf(tu));
+//            const int v0 = static_cast<int>(floorf(tv));
+            const int u0 = std::max(tu, 0);
+            const int v0 = std::max(tv, 0);
+            const int u1 = std::min(tu + 1, m_image->resolution().x() - 1);
+            const int v1 = std::min(tv + 1, m_image->resolution().y() - 1);
 
-            const float s = tu - static_cast<float>(u0);
-            const float t = tv - static_cast<float>(v0);
+            const float s = u * static_cast<float>(m_image->resolution().x() - 1) - static_cast<float>(u0);
+            const float t = v * static_cast<float>(m_image->resolution().y() - 1) - static_cast<float>(v0);
+//            const float s = tu - static_cast<float>(u0);
+//            const float t = tv - static_cast<float>(v0);
 
             const Color texel00 = m_image->get(Point2i(u0, v0));
             const Color texel10 = m_image->get(Point2i(u1, v0));
             const Color texel01 = m_image->get(Point2i(u0, v1));
             const Color texel11 = m_image->get(Point2i(u1, v1));
 
+
+
             const Color interpolated_color = (1 - s) * (1 - t) * texel00
                                              + s * (1 - t) * texel10
                                              + (1 - s) * t * texel01
                                              + s * t * texel11;
             //std::cout<<"interpolated color"<<interpolated_color*m_exposure<<std::endl;
-            return interpolated_color;
+            return interpolated_color*m_exposure;
         }
+        return Color(0.0f,0.0f,0.0f);
     }
 
     std::string toString() const override {
