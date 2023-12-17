957b1ec70c944a4bb8049f5b76fefdb1f5e9d55d
Michal <s8mijure@stud.uni-saarland.de>
2023-12-17 14:57:46 +0100

    e3.1: direct: revert to using early return for emissive surfaces for now

--- src/integrators/direct.cpp
+++ src/integrators/direct.cpp
@@ -12,7 +12,6 @@ public:
 
     Color Li(const Ray& ray, Sampler& rng) override {
         Color result = Color::white();
-        bool hitLight = false;
 
         // First ray
         const Intersection its1 = m_scene->intersect(ray, rng);
@@ -20,16 +19,14 @@ public:
             return m_scene->evaluateBackground(ray.direction).value;
         }
 
-        const BsdfSample bsdfSample = its1.sampleBsdf(rng);
         if (its1.instance->emission() != nullptr) {
-            hitLight = true;
-            result *= its1.evaluateEmission();
             // TODO: ask tutor on why this early return is needed.
             //  If it's not there, in emission.xml you always multiply the light sphere colors by the blue sphere.
-            return result;
-        } else {
-            result *= bsdfSample.weight;
+            return result * its1.evaluateEmission();
         }
+
+        const BsdfSample bsdfSample = its1.sampleBsdf(rng);
+        result *= bsdfSample.weight;
 
 
         // Second ray
@@ -37,35 +34,28 @@ public:
         const Intersection its2 = m_scene->intersect(ray2, rng);
         if (!its2) {
             const Color bgLight = m_scene->evaluateBackground(ray2.direction).value;
-            if (bgLight == Color(0)) {
-                return hitLight ? result : Color::black();
-            }
             return result * bgLight;
         }
 
-        const Color emission = its2.evaluateEmission();
-        if (emission == Color(0)) {
-            return hitLight ? result : Color::black();
-        }
-        if(m_scene->hasLights()){
+        if (m_scene->hasLights()) {
             LightSample light_sampled = m_scene->sampleLight(rng);
-            if(!light_sampled.light->canBeIntersected()){
-
+            if (!light_sampled.light->canBeIntersected()) {
                 Point current_shading_point = its1.position;
-                DirectLightSample direct_light_sampled = light_sampled.light->sampleDirect(current_shading_point,rng);
+                DirectLightSample direct_light_sampled = light_sampled.light->sampleDirect(current_shading_point, rng);
                 Vector light_direction = direct_light_sampled.wi;
                 BsdfEval bsdf = its1.evaluateBsdf(light_direction);
-                Ray Shadow_Ray = {its1.position,light_direction};
-                Intersection shadow_intersect = m_scene->intersect(Shadow_Ray,rng);
-                bool isVisible = shadow_intersect && shadow_intersect.wo.length()>direct_light_sampled.distance;
+                Ray Shadow_Ray = {its1.position, light_direction};
+                Intersection shadow_intersect = m_scene->intersect(Shadow_Ray, rng);
+                bool isVisible = shadow_intersect && shadow_intersect.wo.length() > direct_light_sampled.distance;
                 //Color result_val = bsdf.value;
-                if(isVisible){
-                    result += (bsdf.value*direct_light_sampled.weight)/light_sampled.probability;
+                if (isVisible) {
+                    result += (bsdf.value * direct_light_sampled.weight) / light_sampled.probability;
                 }
                 return result;
             }
         }
-        return result * emission;
+
+        return result * its2.evaluateEmission();
     }
 
     std::string toString() const override {
