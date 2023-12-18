d0d3a099243b88e37b0b636fca686870fcc4d2e9
Michal <s8mijure@stud.uni-saarland.de>
2023-12-18 22:11:46 +0100

    e3.3: fix last bug, clean up code

--- src/bsdfs/dielectric.cpp
+++ src/bsdfs/dielectric.cpp
@@ -2,13 +2,21 @@
 #include <lightwave.hpp>
 
 namespace lightwave {
-
+/**
+ * A dielectric material is one which both reflects and refracts (meaning the ray is transmitted into the inside of the
+ * material). Examples include water, glass, etc. It is also deterministic like a conductor, meaning each wi results in
+ * exactly two wo's.
+ * The refracted light portion is not 'reflected' symmetrically into the inside of the medium due a change in the
+ * speed of light between the two. This skewness is given by the Index of Reflection: the larger the speed difference,
+ * the larger the skewness.
+ * The relative amount of reflected light is given by the fresnel equation, with the amount of refracted light being
+ * equal to 1 - F.
+ */
 class Dielectric : public Bsdf {
 private:
     ref<Texture> m_ior;
     ref<Texture> m_reflectance;
     ref<Texture> m_transmittance;
-    Vector m_normal = {0.0f, 0.0f, 1.0f};
 
 public:
     explicit Dielectric(const Properties& properties) {
@@ -26,30 +34,39 @@ public:
         return BsdfEval::invalid();
     }
 
+    /**
+     * Since we don't want to trace two rays, we instead sample the reflected/refracted one based on the relative
+     * amount of light going in that direction. Since the BSDF is symmetric, it doesn't matter that we're starting out
+     * with wi. The @c fresnelDielectric function also takes care of the total internal reflection case by returning
+     * a 1 in that case (thus taking up the entire rng spectrum). Finally, if we're coming from inside instead of the
+     * outside of the material, we need to flip the ior/eta and the normal.
+     */
     BsdfSample sample(const Point2& uv, const Vector& wo, Sampler& rng) const override {
         float eta;
         Vector normal;
-        // coming from outside
         if (wo.z() >= 0.0f) {
+            // wo is coming from outside the material
             eta = m_ior->scalar(uv);
             normal = {0.0f, 0.0f, 1.0f};
         } else {
+            // wo is coming from inside the material
             eta = 1 / m_ior->scalar(uv);
             normal = {0.0f, 0.0f, -1.0f};
         }
 
+        Vector wi;
+        Color weight;
         const float reflectProbability = fresnelDielectric(wo.z(), eta);
         if (rng.next() <= reflectProbability) {
             // reflect
-            const Vector wi = reflect(wo, normal);
-            const Color weight = m_reflectance->evaluate(uv);
-            return {wi, weight};
+            wi = reflect(wo, normal);
+            weight = m_reflectance->evaluate(uv);
         } else {
             // refract
-            const Vector wi = refract(wo, normal, eta);
-            const Color weight = m_transmittance->evaluate(uv);
-            return {wi, weight};
+            wi = refract(wo, normal, eta);
+            weight = m_transmittance->evaluate(uv) / sqr(eta);
         }
+        return {wi, weight};
     }
 
     std::string toString() const override {
